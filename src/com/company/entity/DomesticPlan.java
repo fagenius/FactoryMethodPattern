4f99dab282e50e84f3c305cfc450207d1b0a69b0
fagenius <ebuyamak@gmail.com>
2022-07-18 21:56:11 +0000

    Design patter Factory metho

---
+++ src/com/company/entity/DomesticPlan.java
@@ -0,0 +1,8 @@
+package com.company.entity;
+
+public class DomesticPlan extends Plan{
+    @Override
+    public void getRate() {
+        rate = 3.05;
+    }
+}
---
+++ src/com/company/entity/Plan.java
@@ -0,0 +1,12 @@
+package com.company.entity;
+
+public abstract class Plan {
+
+    protected double rate;
+
+    public abstract void getRate();
+
+    public void calculateBill(int units){
+        System.out.println(units*rate);
+    }
+}
